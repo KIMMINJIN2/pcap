898f1bef1cdb5e40222affb1d616a911ff19b9e4
mj <mjmj419@korea.ac.kr>
2016-09-22 03:30:05 +0900

    hw

--- main.c
+++ main.c
@@ -139,47 +139,12 @@ void print_tcp_packet(const u_char * Buffer, int Size)
   
 void Print_data (const u_char * data , int Size)
 {
+
     int i , j;
     for(i=0 ; i < Size ; i++)
-    {/*
-        if( i!=0 && i%16==0)   // one line
-        {
-            printf( "         ");
-            for(j=i-16 ; j<i ; j++)
-            {
-                if(data[j]>=32 && data[j]<=128)
-                    printf( "%c",(unsigned char)data[j]); //if its a number or alphabet
-                 
-                else printf( "."); //otherwise print a dot
-            }
-            printf(  "\n");
-        }*/
-         
-        if(i%16==0) printf(  "   ");
-            printf( " %02X",(unsigned int)data[i]);
-                 
-        if( i==Size-1)  //print the last spaces
-        {
-            for(j=0;j<15-i%16;j++)
-            {
-              printf( "\n "); //extra spaces
-            }
-             
-            //printf( "\n");
-             
-            for(j=i-i%16 ; j<=i ; j++)
-            {
-                if(data[j]>=32 && data[j]<=128)
-                {
-                  printf( "%c",(unsigned char)data[j]);
-                }
-                else
-                {
-                  printf( ".");
-                }
-            }
-             
-            printf( "\n" );
-        }
+    {
+        if(i%16==0) printf("\n   ");
+        printf( " %02X",(unsigned int)data[i]);
     }
+
 }
